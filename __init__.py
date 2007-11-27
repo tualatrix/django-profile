403eb11a651527d7532df1937c2ddaaf72f56b1c
david.rubert <david.rubert@7b2eb738-463f-0410-8820-43b14e3dcc91>
2007-11-27 17:07:14 +0000

    Reestructuring templates
    git-svn-id: http://django-profile.googlecode.com/svn/trunk/profile@39 7b2eb738-463f-0410-8820-43b14e3dcc91

--- __init__.py
+++ __init__.py
@@ -4,6 +4,8 @@ import sys
 if len(sys.argv) > 1 and sys.argv[0] == "manage.py" and sys.argv[1] == "runserver":
     # Import the initial data in the Country and Continent database
     if len(Continent.objects.all()) == 0:
+        print "Importing  continents..."
         Continent().importdata()
     if len(Country.objects.all()) == 0:
+        print "Importing  countries..."
         Country().importdata()
