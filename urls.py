0d00b844106e64688094fbe490bd795def952e79
david.rubert <david.rubert@7b2eb738-463f-0410-8820-43b14e3dcc91>
2008-04-16 16:02:49 +0000

    More changes to allow the e-mail validation
    git-svn-id: http://django-profile.googlecode.com/svn/trunk/userprofile@124 7b2eb738-463f-0410-8820-43b14e3dcc91

--- urls.py
+++ urls.py
@@ -5,17 +5,17 @@ from django.conf import settings
 
 urlpatterns = patterns('',
     # Private profile
-    (r'^$', private, {'APIKEY': settings.APIKEY, 'template': 'profile/private.html'}),
+    (r'^$', private, {'APIKEY': settings.APIKEY, 'template': 'userprofile/private.html'}),
     (r'^save/$', save),
-    (r'^delete/$', delete, {'template': 'profile/delete.html'}),
-    (r'^delete/done/$', direct_to_template, {'template': 'profile/delete_done.html'}),
+    (r'^delete/$', delete, {'template': 'userprofile/delete.html'}),
+    (r'^delete/done/$', direct_to_template, {'template': 'userprofile/delete_done.html'}),
     (r'^avatar/delete/$', avatarDelete),
     (r'^avatar/delete/(?P<avatar_id>[0-9]+)/$', avatarDelete),
-    (r'^avatar/choose/$', avatarChoose, {'template': 'profile/avatar_choose.html'}),
-    (r'^avatar/crop/(?P<avatar_id>[0-9]+)/$', avatarCrop, {'template': 'profile/avatar_crop.html'}),
+    (r'^avatar/choose/$', avatarChoose, {'template': 'userprofile/avatar_choose.html'}),
+    (r'^avatar/crop/(?P<avatar_id>[0-9]+)/$', avatarCrop, {'template': 'userprofile/avatar_crop.html'}),
     (r'^getcountry_info/(?P<lat>[0-9\.\-]+)/(?P<lng>[0-9\.\-]+)/$', fetch_geodata),
 
     # Public profile
-    (r'^users/(?P<user>[a-zA-Z0-9\-_]*)/$', public, {'APIKEY': settings.APIKEY, 'template': 'profile/public.html'}),
+    (r'^users/(?P<user>[a-zA-Z0-9\-_]*)/$', public, {'APIKEY': settings.APIKEY, 'template': 'userprofile/public.html'}),
 
 )
