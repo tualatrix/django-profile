b6ad0cfe42483c65b7741f1e4b11c8603e5efedd
david.rubert <david.rubert@7b2eb738-463f-0410-8820-43b14e3dcc91>
2008-06-03 14:48:54 +0000

    Removed the libmagic dependency
    git-svn-id: http://django-profile.googlecode.com/svn/trunk/userprofile@192 7b2eb738-463f-0410-8820-43b14e3dcc91

--- forms.py
+++ forms.py
@@ -3,7 +3,6 @@ from django.core.exceptions import ObjectDoesNotExist
 from userprofile.models import Profile, Avatar, GENDER_CHOICES
 from django.utils.translation import ugettext as _
 from userprofile.models import Country
-import magic
 
 IMAGE_TYPES = { 'JPEG image data': '.jpg', 'PNG image data': '.png', 'GIF image data': '.gif' }
 
@@ -21,17 +20,6 @@ class AvatarForm(forms.Form):
     The avatar form requires only one image field.
     """
     photo = forms.ImageField()
-
-    def clean_photo(self):
-        photo = self.cleaned_data.get('photo')
-        ms = magic.open(magic.MAGIC_NONE)
-        ms.load()
-        type = ms.buffer(photo.content)
-
-        if not type.split(",")[0] in IMAGE_TYPES:
-            raise forms.ValidationError(_('The file type is invalid: %s' % type))
-
-        return { 'photo': photo, 'extension': IMAGE_TYPES.get(type.split(",")[0]) }
 
 class AvatarCropForm(forms.Form):
     """
