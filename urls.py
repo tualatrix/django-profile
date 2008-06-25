2b68c44ecaf6d0f8a437a75fe8da5cca78b3edf2
david.rubert <david.rubert@7b2eb738-463f-0410-8820-43b14e3dcc91>
2008-06-25 09:20:57 +0000

    Complete reestructuration. Doesn't work for the moment
    git-svn-id: http://django-profile.googlecode.com/svn/trunk/userprofile@262 7b2eb738-463f-0410-8820-43b14e3dcc91

--- urls.py
+++ urls.py
@@ -9,25 +9,25 @@ WEBSEARCH = hasattr(settings, "WEBSEARCH") and settings.WEBSEARCH or None
 
 urlpatterns = patterns('',
     # Private profile
-    (r'^profile/$', overview, { 'section': 'overview', 'APIKEY': APIKEY, 'template': 'userprofile/overview.html'}),
-    (r'^profile/edit/(?P<section>location)/$', profile, {'APIKEY': APIKEY, 'template': 'userprofile/location.html'}),
-    (r'^profile/edit/(?P<section>personal)/$', profile, {'template': 'userprofile/personal.html'}),
-    (r'^profile/edit/(?P<section>personal|location|public)/done/$', direct_to_template, {'template': 'userprofile/profile_done.html'}),
-    (r'^profile/delete/$', delete, { 'section': 'delete', 'template': 'userprofile/delete.html'}),
-    (r'^profile/delete/done/$', direct_to_template, { 'section': 'delete', 'template': 'userprofile/delete_done.html'}),
-    (r'^profile/edit/public/$', makepublic, { 'section': 'makepublic', 'APIKEY': APIKEY, 'template': 'userprofile/makepublic.html'}),
-    (r'^profile/edit/avatar/$', avatarchoose, { 'section': 'avatar', 'websearch': WEBSEARCH, 'template': 'userprofile/avatar_choose.html'}),
+    (r'^profile/$', overview, { 'section': 'overview', 'APIKEY': APIKEY, 'template': 'userprofile/profile/overview.html'}),
+    (r'^profile/edit/(?P<section>location)/$', profile, {'APIKEY': APIKEY, 'template': 'userprofile/profile/location.html'}),
+    (r'^profile/edit/(?P<section>personal)/$', profile, {'template': 'userprofile/profile/personal.html'}),
+    (r'^profile/edit/(?P<section>personal|location|public)/done/$', direct_to_template, {'template': 'userprofile/profile/profile_done.html'}),
+    (r'^profile/delete/$', delete, { 'section': 'delete', 'template': 'userprofile/profile/delete.html'}),
+    (r'^profile/delete/done/$', direct_to_template, { 'section': 'delete', 'template': 'userprofile/profile/delete_done.html'}),
+    (r'^profile/edit/public/$', makepublic, { 'section': 'makepublic', 'APIKEY': APIKEY, 'template': 'userprofile/profile/makepublic.html'}),
+    (r'^profile/edit/avatar/$', avatarchoose, { 'section': 'avatar', 'websearch': WEBSEARCH, 'template': 'userprofile/avatar/avatar_choose.html'}),
     (r'^profile/edit/avatar/delete/$', avatardelete),
-    (r'^profile/edit/avatar/search/$', searchimages, { 'section': 'avatar', 'template': 'userprofile/avatar_search.html'}),
-    (r'^profile/edit/avatar/crop/$', avatarcrop, { 'section': 'avatar', 'template': 'userprofile/avatar_crop.html'}),
-    (r'^profile/edit/avatar/crop/done/$', direct_to_template, { 'section': 'avatar', 'template': 'userprofile/avatar_done.html'}),
+    (r'^profile/edit/avatar/search/$', searchimages, { 'section': 'avatar', 'template': 'userprofile/avatar/avatar_search.html'}),
+    (r'^profile/edit/avatar/crop/$', avatarcrop, { 'section': 'avatar', 'template': 'userprofile/avatar/avatar_crop.html'}),
+    (r'^profile/edit/avatar/crop/done/$', direct_to_template, { 'section': 'avatar', 'template': 'userprofile/avatar/avatar_done.html'}),
     (r'^profile/getcountry_info/(?P<lat>[0-9\.\-]+)/(?P<lng>[0-9\.\-]+)/$', fetch_geodata),
 
     # Public profile
-    (r'^profile/(?P<current_user>[a-zA-Z0-9\-_]*)/$', public, {'APIKEY': APIKEY, 'template': 'userprofile/public.html'}),
+    (r'^profile/(?P<current_user>[a-zA-Z0-9\-_]*)/$', public, {'APIKEY': APIKEY, 'template': 'userprofile/profile/public.html'}),
 
     # Account utilities
-    (r'^manage/$', overview, { 'section': 'overview', 'template' : 'userprofile/overview.html'}),
+    (r'^manage/$', overview, { 'section': 'overview', 'template' : 'userprofile/profile/overview.html'}),
     (r'^password/reset/$', reset_password, {'template' : 'userprofile/password_reset.html'}),
     (r'^validation/reset/$', validation_reset, {'template' : 'userprofile/validation_reset.html'}),
     (r'^validation/reset/done/(?P<action>success|failed)/$', direct_to_template, {'template' : 'userprofile/validation_reset_done.html'}),
