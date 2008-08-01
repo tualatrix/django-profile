f3a7da3fd1fe9e8f0c6f1c3d484cb86c2501f83f
alvaro.stevens <alvaro.stevens@7b2eb738-463f-0410-8820-43b14e3dcc91>
2008-08-01 08:33:01 +0000

    Changed all the tag. The problem was that it didn't resize the default avatar if it was bigger than the requested size. I stripped it and created several 'mini-functions' to simplify the main renderer.
    git-svn-id: http://django-profile.googlecode.com/svn/trunk/userprofile@293 7b2eb738-463f-0410-8820-43b14e3dcc91

--- templatetags/avatars.py
+++ templatetags/avatars.py
@@ -24,103 +24,104 @@ class ResizedThumbnailNode(Node):
             self.size = Variable(size)
         self.user = username
 
-    def render(self, context):
-        # If size is not an int, then it's a Variable, so try to resolve it.
+    def get_user(self, context):
         # If there's a username, go get it! Otherwise get the current.
-        try:
-            if not isinstance(self.size, int):
-                self.size = int(self.size.resolve(context))
-            if self.user:
-                try:
-                    self.user = User.objects.get(username=self.user)
-                except:
-                    self.user = Variable(self.user).resolve(context)
-            else:
-                self.user = Variable('user').resolve(context)
-        except Exception, e:
-            print e
-            return '' # just die...
-        size_equals = self.size == _settings.DEFAULT_AVATAR_WIDTH
-        if self.size > _settings.DEFAULT_AVATAR_WIDTH:
-            return '' # unacceptable
+        if self.user:
+            try:
+                user = User.objects.get(username=self.user)
+            except:
+                user = Variable(self.user).resolve(context)
+        else:
+            user = Variable('user').resolve(context)
+        return user
+
+    def size_equals(self, file=None):
+        if not file:
+            return self.size == _settings.DEFAULT_AVATAR_WIDTH
+        else:
+            return self.size == Image(file).size().width()
+
+    def get_profile(self):
         # Maybe django-profile it's not set as AUTH_PROFILE_MODULE
         try:
             profile = self.user.get_profile()
         except Exception, e:
             print e
             profile = Profile.objects.get(user=self.user)
+        return profile
+
+    def get_filename(self, profile=None):
         # For compatibility with the official django-profile model I check
         # whether it's a path or just a filename.
         # In my opinion in the database should only be saved the file name,
         # and all files be stored in a standard directory:
         # settings.AVATAR_DIRS[int]/str(User)/settings_DEFAULT_AVATAR_WIDTH/
+        default = False
         try:
-            filename = profile.avatar[profile.avatar.rindex('/')+1:]
+            file_name = profile.avatar[profile.avatar.rindex('/')+1:]
         except:
-            if profile.avatar:
-                filename = profile.avatar
+            if not profile is None and profile.avatar:
+                file_name = profile.avatar
             else:
-                filename = _settings.DEFAULT_AVATAR
+                file_name = _settings.DEFAULT_AVATAR
+                default = True
+        return (file_name, default)
+
+    def as_url(self, path):
+        try:
+            return path.replace(settings.MEDIA_ROOT, settings.MEDIA_URL)
+        except:
+            return ''
+
+    def render(self, context):
+        try:
+            # If size is not an int, then it's a Variable, so try to resolve it.
+            if not isinstance(self.size, int):
+                self.size = int(self.size.resolve(context))
+            self.user = self.get_user(context)
+        except Exception, e:
+            print e
+            return '' # just die...
+        if self.size > _settings.DEFAULT_AVATAR_WIDTH:
+            return '' # unacceptable
+        profile = self.get_profile()
         # Avatar's heaven, where all the avatars go.
         avatars_root = path.join(_settings.AVATARS_DIR,
                                  slugify(self.user.username))
         file_root = path.join(avatars_root, str(self.size))
+        file_name, defaulting = self.get_filename(profile)
+        if defaulting:
+            file_root = _settings.AVATARS_DIR
+            if self.size_equals():
+                return self.as_url(path.join(file_root, file_name))
         if not path.exists(file_root):
             makedirs(file_root)
-        file_path = '%(root)s/%(file)s' % {'root': file_root, 'file': filename}
-        print 'file_path:', file_path
+        file_path = path.join(file_root, file_name)
         # I can't return an absolute path... can I?
-        file_url = file_path.replace(settings.MEDIA_ROOT, settings.MEDIA_URL)
-        if path.exists(file_path):
-            return file_url
+        if not defaulting:
+            if path.exists(file_path):
+                file_url = self.as_url(file_path)
+                return file_url
+            else:
+                if not profile.avatar:
+                    file_root = _settings.AVATARS_DIR
+                    file_path = path.join(file_root, _settings.DEFAULT_AVATAR)
         # Oops, I din't find it, let's try to generate it.
-        orig_root = path.join(avatars_root, 
-                              str(_settings.DEFAULT_AVATAR_WIDTH))
-        # The OS path to the image file.
-        orig_path = path.join(file_root, filename)
-        # Look for the original avatar in the standard directory, if found,
-        # save the location for later use.
-        if not path.exists(orig_path):
+        if path.exists(file_path):
+            orig_file = Image(file_path)
+            dest_root = path.join(avatars_root, str(self.size))
             try:
-                makedirs(orig_root)
+                makedirs(dest_root)
             except Exception, e:
                 print e
-                # Nothing was found... let's try something else.
-                pass
-            # For compatibility with the official model, I check if the old
-            # path exists. If it does, I copy it to the new standard location.
-            if profile.avatar:
-                orig_path = path.join(settings.MEDIA_ROOT, profile.avatar)
-                if path.exists(orig_path):
-                    orig_file = Image(orig_path)
-                    print orig_path
-                    dest_root = path.join(avatars_root,
-                                          str(orig_file.size().width()))
-                    try:
-                        makedirs(dest_root)
-                        copy(orig_path, dest_root)
-                    except Exception, e:
-                        print e
-                        return ''
-                    # I save the new orig_path for later.
-                    orig_path = path.join(dest_root, filename)
-                # Did my best...
-                else:
-                    return '' # fail silently
-            else:
-                return path.join(
-                    _settings.AVATARS_DIR, _settings.DEFAULT_AVATAR).replace(
-                        settings.MEDIA_ROOT, settings.MEDIA_URL)
+            # Save the new path for later...
+            dest_path = path.join(dest_root, file_name)
         else:
-            # I found it, and was already scaled! Great news.
-            if size_equals:
-                return orig_path.replace(settings.MEDIA_ROOT, \
-                                          settings.MEDIA_URL)
-        orig_file = Image(orig_path)
+            # Did my best...
+            return '' # fail silently
         orig_file.scale(self.size)
-        # file_path, what we were looking for in the first place... remember?
-        if orig_file.write(file_path):
-            return file_url
+        if orig_file.write(dest_path):
+            return self.as_url(dest_path)
         else:
             print '=== ERROR ==='
             return '' # damn! Close but no cigar...
