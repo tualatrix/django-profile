7a35615a84bcdcbfcd338b4bf0d65c6683efcc0c
alvaro.stevens <alvaro.stevens@7b2eb738-463f-0410-8820-43b14e3dcc91>
2008-08-03 22:19:11 +0000

    URLs translated into spanish.
    git-svn-id: http://django-profile.googlecode.com/svn/trunk/userprofile@298 7b2eb738-463f-0410-8820-43b14e3dcc91

--- urls/es.py
+++ urls/es.py
@@ -9,155 +9,155 @@ WEBSEARCH = hasattr(settings, "WEBSEARCH") and settings.WEBSEARCH or None
 
 urlpatterns = patterns('',
     # Private profile
-    url(r'^profile/$', overview,
+    url(r'^perfil/$', overview,
         {'section': 'overview', 'APIKEY': APIKEY,
          'template': 'userprofile/profile/overview.html'},
         name='profile_overview'),
 
-    url(r'^profile/for/(?P<username>.+)/$', overview,
+    url(r'^perfil/de/(?P<username>.+)/$', overview,
         {'section': 'overview', 'APIKEY': APIKEY,
          'template': 'userprofile/profile/overview.html'},
         name='profile_overview'),
 
-    url(r'^profile/edit/location/$', location,
+    url(r'^perfil/editar/ubicacion/$', location,
         {'section': 'location', 'APIKEY': APIKEY, 'template': 'userprofile/profile/location.html'},
         name='profile_edit_location'),
 
-    url(r'^profile/edit/personal/$', personal,
+    url(r'^perfil/editar/personal/$', personal,
         {'section': 'personal', 'template': 'userprofile/profile/personal.html'},
         name='profile_edit_personal'),
 
-    url(r'^profile/edit/location/done/$', direct_to_template,
+    url(r'^perfil/editar/ubicacion/listo/$', direct_to_template,
         {'extra_context': {'section': 'location'},
         'template': 'userprofile/profile/location_done.html'},
         name='profile_edit_location_done'),
 
-    url(r'^profile/edit/personal/done/$', direct_to_template,
+    url(r'^perfil/editar/personal/listo/$', direct_to_template,
         {'extra_context': {'section': 'personal'},
         'template': 'userprofile/profile/personal_done.html'},
         name='profile_edit_personal_done'),
 
-    url(r'^profile/edit/public/$', makepublic,
+    url(r'^perfil/editar/publico/$', makepublic,
         {'section': 'makepublic', 'APIKEY': APIKEY,
          'template': 'userprofile/profile/makepublic.html'},
         name='profile_edit_public'),
 
-    url(r'^profile/edit/public/done/$', direct_to_template,
+    url(r'^perfil/editar/publico/listo/$', direct_to_template,
         {'extra_context': {'section': 'public'},
         'template': 'userprofile/profile/makepublic_done.html'},
-        name='profile_edit_public_done'),
+        name='profile_edit_public_done'),          
 
-    url(r'^profile/delete/$', delete,
+    url(r'^perfil/eliminar/$', delete,
         {'section': 'delete', 'template': 'userprofile/profile/delete.html'},
         name='profile_delete'),
 
-    url(r'^profile/delete/done/$', direct_to_template,
+    url(r'^perfil/eliminar/listo/$', direct_to_template,
         {'section': 'delete',
          'template': 'userprofile/profile/delete_done.html'},
         name='profile_delete_done'),
 
-    url(r'^profile/edit/avatar/$', avatarchoose,
+    url(r'^perfil/editar/avatar/$', avatarchoose,
         {'section': 'avatar', 'websearch': WEBSEARCH,
          'template': 'userprofile/avatar/choose.html'},
         name='profile_edit_avatar'),
 
-    url(r'^profile/edit/avatar/delete/$', avatardelete,
+    url(r'^perfil/editar/avatar/eliminar/$', avatardelete,
         name='profile_avatar_delete'),
 
-    url(r'^profile/edit/avatar/search/$', searchimages,
+    url(r'^perfil/editar/avatar/buscar/$', searchimages,
         {'section': 'avatar', 'template': 'userprofile/avatar/search.html'},
         name='profile_avatar_search'),
 
-    url(r'^profile/edit/avatar/crop/$', avatarcrop,
+    url(r'^perfil/editar/avatar/recortar/$', avatarcrop,
         {'section': 'avatar', 'template': 'userprofile/avatar/crop.html'},
         name='profile_avatar_crop'),
 
-    url(r'^profile/edit/avatar/crop/done/$', direct_to_template,
+    url(r'^perfil/editar/avatar/recortar/listo/$', direct_to_template,
         {'section': 'avatar', 'template': 'userprofile/avatar/done.html'},
         name='profile_avatar_crop_done'),
 
-    url(r'^profile/getcountry_info/(?P<lat>[0-9\.\-]+)/(?P<lng>[0-9\.\-]+)/$',
+    url(r'^perfil/obtenerpais_info/(?P<lat>[0-9\.\-]+)/(?P<lng>[0-9\.\-]+)/$',
         fetch_geodata,
         name='profile_geocountry_info'),
 
 
     # Public profile
-    url(r'^profile/(?P<current_user>[a-zA-Z0-9\-_]*)/$', public,
+    url(r'^perfil/(?P<current_user>[a-zA-Z0-9\-_]*)/$', public,
         {'APIKEY': APIKEY, 'template': 'userprofile/profile/public.html'},
         name=''),
 
 
     # Account utilities
-    url(r'^manage/$', overview,
+    url(r'^administrar/$', overview,
         {'section': 'overview',
          'template' : 'userprofile/profile/overview.html'},
         name='account_overview'),
 
-    url(r'^password/reset/$', reset_password,
+    url(r'^password/reestablecer/$', reset_password,
         {'template' : 'userprofile/account/password_reset.html'},
         name='password_reset'),
 
-    url(r'^password/reset/done/$', direct_to_template,
+    url(r'^password/reestablecer/listo/$', direct_to_template,
         {'template': 'userprofile/account/password_reset_done.html'},
         name='password_reset_done'),
 
-    url(r'^email/validation/$', email_validation,
+    url(r'^email/validar/$', email_validation,
         {'section': 'overview',
          'template': 'userprofile/account/email_validation.html'},
         name='email_validation'),
 
-    url(r'^email/validation/processed/$', direct_to_template,
+    url(r'^email/validar/procesado/$', direct_to_template,
         {'extra_context': {'section': 'overview'},
          'template': 'userprofile/account/email_validation_processed.html'},
          name='email_validation_processed'),
 
-    url(r'^email/validation/(?P<key>.{70})/$', email_validation_process,
+    url(r'^email/validar/(?P<key>.{70})/$', email_validation_process,
         {'section': 'overview',
          'template': 'userprofile/account/email_validation_done.html'},
         name='email_validation_process'),
 
-    url(r'^email/validation/reset/$', email_validation_reset,
+    url(r'^email/validar/reestablecer/$', email_validation_reset,
         {'template' : 'userprofile/account/email_validation_reset.html'},
         name='email_validation_reset'),
 
-    url(r'^email/validation/reset/done/(?P<action>success|failed)/$',
+    url(r'^email/validar/reestablecer/listo/(?P<action>success|failed)/$',
         direct_to_template, {'extra_context': {'section': 'overview'},
         'template' : 'userprofile/account/email_validation_reset_done.html'},
         name='email_validation_reset_done'),
 
-    url(r'^password/change/$', change_password_authenticated,
+    url(r'^password/cambiar/$', change_password_authenticated,
         {'section': 'overview',
          'template': 'userprofile/account/password_change.html'},
         name='password_change'),
 
-    url(r'^password/change/done/$', direct_to_template,
+    url(r'^password/cambiar/listo/$', direct_to_template,
         {'extra_context': {'section': 'overview'},
          'template': 'userprofile/account/password_change_done.html'},
         name='password_change_done'),
 
-    url(r'^password/change/(?P<key>.{70})/$', change_password_with_key,
+    url(r'^password/cambiar/(?P<key>.{70})/$', change_password_with_key,
         {'template': 'userprofile/account/password_change.html'},
         name='password_change_with_key'),
 
-    url(r'^login/$', views.login,
+    url(r'^entrar/$', views.login,
         {'template_name': 'userprofile/account/login.html'},
         name='login'),
 
-    url(r'^logout/$', logout,
+    url(r'^salir/$', logout,
         {'template': 'userprofile/account/logout.html'},
         name='logout'),
 
 
     # Registration
-    url(r'^register/$', register,
+    url(r'^registro/$', register,
         {'template' : 'userprofile/account/registration.html'},
         name='signup'),
 
-    url(r'^register/validate/$', direct_to_template,
+    url(r'^registro/validar/$', direct_to_template,
         {'template' : 'userprofile/account/validate.html'},
         name='signup_validate'),
 
-    url(r'^register/complete/$', direct_to_template,
+    url(r'^registro/completo/$', direct_to_template,
         {'template': 'userprofile/account/registration_done.html'},
         name='signup_complete'),
 )
