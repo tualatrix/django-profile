baa9f05713d1ab4efcf5bec9e63fd506d7586214
david.rubert <david.rubert@7b2eb738-463f-0410-8820-43b14e3dcc91>
2008-08-05 19:52:38 +0000

    Solved some issues on the import funcion
    git-svn-id: http://django-profile.googlecode.com/svn/trunk/userprofile@309 7b2eb738-463f-0410-8820-43b14e3dcc91

--- urls/__init__.py
+++ urls/__init__.py
@@ -1,14 +1,13 @@
 from django.conf import settings
-
 from os.path import abspath, basename, dirname, join
-import sys
 
 directory = dirname(abspath(__file__))
 PROJECT_ABSOLUTE_DIR = directory[:directory.rfind('/')]
 PROJECT_NAME = basename(PROJECT_ABSOLUTE_DIR)
 
-if hasattr(settings, 'LANGUAGE_CODE'):
-    __import__(PROJECT_NAME + '.urls.' + settings.LANGUAGE_CODE,
-               fromlist=['urlpatterns'])
-else:
-    from en import urlpatterns
+try:
+    module = __import__("%s.urls.%s" % (PROJECT_NAME, settings.LANGUAGE_CODE), fromlist=["urlpatterns"])
+    globals().update({ "urlpatterns": module.__dict__["urlpatterns"] })
+except:
+    module = __import__("%s.urls.en" % (PROJECT_NAME), fromlist=["urlpatterns"])
+    globals().update({ "urlpatterns": module.__dict__["urlpatterns"] })
