e301c5c4c1bb459e8c100906fae94acdf0a03d81
david.rubert <david.rubert@7b2eb738-463f-0410-8820-43b14e3dcc91>
2008-08-19 19:30:04 +0000

    Removed some fuzzy import code
    git-svn-id: http://django-profile.googlecode.com/svn/trunk/userprofile@331 7b2eb738-463f-0410-8820-43b14e3dcc91

--- urls/__init__.py
+++ urls/__init__.py
@@ -1,18 +1,13 @@
 from django.conf import settings
-from os.path import abspath, basename, dirname, join
-
-directory = dirname(abspath(__file__))
-PROJECT_ABSOLUTE_DIR = directory[:directory.rfind('/')]
-PROJECT_NAME = basename(PROJECT_ABSOLUTE_DIR)
 
 if hasattr(settings, "I18N_URLS") and settings.I18N_URLS:
     try:
-        module = __import__("%s.urls.%s" % (PROJECT_NAME, \
-                            settings.LANGUAGE_CODE), {}, {}, "urlpatterns")
+        module = __import__("userprofile.urls.%s" % (settings.LANGUAGE_CODE), \
+                            {}, {}, "urlpatterns")
         globals().update({ "urlpatterns": module.__dict__["urlpatterns"] })
     except:
-        module = __import__("%s.urls.en" % (PROJECT_NAME), {}, {}, "urlpatterns")
+        module = __import__("userprofile.urls.en", {}, {}, "urlpatterns")
         globals().update({ "urlpatterns": module.__dict__["urlpatterns"] })
 else:
-    module = __import__("%s.urls.en" % (PROJECT_NAME), {}, {}, "urlpatterns")
+    module = __import__("userprofile.urls.en", {}, {}, "urlpatterns")
     globals().update({ "urlpatterns": module.__dict__["urlpatterns"] })
