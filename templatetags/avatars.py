b6c3e738a5df26b79a2d759d8e7050b4b5a1760c
david.rubert <david.rubert@7b2eb738-463f-0410-8820-43b14e3dcc91>
2008-12-08 15:43:52 +0000

    Solved a bug on the avatar template tag
    git-svn-id: http://django-profile.googlecode.com/svn/trunk/userprofile@381 7b2eb738-463f-0410-8820-43b14e3dcc91

--- templatetags/avatars.py
+++ templatetags/avatars.py
@@ -69,7 +69,7 @@ class ResizedThumbnailNode(Node):
 @register.tag('avatar')
 def Thumbnail(parser, token):
     bits = token.contents.split()
-    username = None
+    username = "request.user"
     if len(bits) > 3:
         raise TemplateSyntaxError, _(u"You have to provide only the size as \
             an integer (both sides will be equal) and optionally, the \
