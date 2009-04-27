8a03f47c839b257d6601e182a3dbab4057c61bfa
stephane.angel <stephane.angel@7b2eb738-463f-0410-8820-43b14e3dcc91>
2009-04-27 23:13:53 +0000

    update of commit r405: correct a bug if a signal has multiple receivers and not the last one return a value
    git-svn-id: http://django-profile.googlecode.com/svn/trunk/userprofile@406 7b2eb738-463f-0410-8820-43b14e3dcc91

--- signals.py
+++ signals.py
@@ -6,9 +6,12 @@ context_signal = Signal(providing_args=['request', 'context'])
 #callback *could* return a new http response object
 post_signal = Signal(providing_args=['request', 'form', 'extra'])
 
-def last_response(responses):
-    """ get the last response value from responses of a signal """
+def last_response(responses, allow_none=False):
+    """ get the last existing response value from responses of a signal """
     try:
-        return responses[-1][1]
+        if allow_none:
+            return responses[-1][1]
+        else:
+            return [response for response in responses if response[1] is not None][-1][1]
     except:
         return None
